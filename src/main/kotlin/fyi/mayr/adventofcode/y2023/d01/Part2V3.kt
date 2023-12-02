112ed255154f5e1139b644df8356bf8fb8b9732e
Janik Mayr <32466915+jan222ik@users.noreply.github.com>
2023-12-02 02:01:30 +0100

    day01

--- src/main/kotlin/fyi/mayr/adventofcode/y2023/d01/Part2V3.kt
+++ src/main/kotlin/fyi/mayr/adventofcode/y2023/d01/Part2V3.kt
@@ -7,20 +7,24 @@ fun main(args: Array<String>) {
     println("Part 2V3: sum = ${part2(lines)}")
 }
 
-private const val REGEX = "(one|1)|(two|2)|(three|3)|(four|4)|(five|5)|(six|6)|(seven|7)|(eight|8)|(nine|9)"
-
-private fun part2(lines: List<String>) = lines.sumOf {
-    "${firstMatchDigit(it, REGEX)}${firstMatchDigit(it, "(?:.*)(?:$REGEX)+(?:.*?)")}".toInt()
-}
-
+private const val DIGITS_REGEX = "(one|1)|(two|2)|(three|3)|(four|4)|(five|5)|(six|6)|(seven|7)|(eight|8)|(nine|9)"
 /*
-   Match Last Digit Pattern Explanation:
+   Last Digit Pattern Explanation:
    (?:.*) : Non-capturing group of greedy capture
    (?:$REGEX)+ : Non-capturing group of normal pattern with at least one occurrence, the latest matched REGEX capture group will be returned in find.
    (?:.*?) : Non-capturing group of reluctant word end to force greedy capture to expand
 */
+private const val LAST_DIGIT_REGEX = "(?:.*)(?:$DIGITS_REGEX)+(?:.*?)"
 
+private fun part2(lines: List<String>) = lines.sumOf {
+    firstMatchDigit(it, DIGITS_REGEX) * 10 + firstMatchDigit(it, LAST_DIGIT_REGEX)
+}
+
+/**
+ * Gets the first matching digit by getting the index of the matching capture group, skipping the first capture group.
+ */
 private fun firstMatchDigit(input: String, regexStr: String): Int {
     return regexStr.toRegex().find(input)?.groups?.indexOfLast { it != null } ?: error("unreachable")
+    // use indexOfLast because group[0] is the whole match, therefore going from the back to the front.
 }
 
